e553d777f20fe9b72abb82f63416703a80a2a9c4
HP <2134514170@qq.com>
2025-05-26 22:10:45 +0800

    day01 learning

--- day01/student.cpp
+++ day01/student.cpp
@@ -1,45 +1,58 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include<iostream>
 
 class Student {
 public:
-    Student() {
-        cout << "Default constructor\n";
-    }
-
-    Student(const string& name, int age) : name_(name), age_(age) {
-        cout << "Parameterized constructor\n";
-    }
-
-    Student(const Student& other) {
-        name_ = other.name_;
-        age_ = other.age_;
-        cout << "Copy constructor\n";
-    }
-
-    Student& operator=(const Student& other) {
-        if (this != &other) {
-            name_ = other.name_;
-            age_ = other.age_;
-            cout << "Copy assignment\n";
-        }
-        return *this;
-    }
-
-    ~Student() {
-        cout << "Destructor\n";
-    }
-
+	Student(): m_name(""), m_age(0) {
+		std::cout << "默认构造被调用" << std::endl;
+	}
+	Student(const std::string& in_name, int in_age): m_name(in_name), m_age(in_age) {
+		std::cout << "带参构造被调用" << std::endl;
+	}
+	Student(const Student& other): m_name(other.m_name), m_age(other.m_age) {
+		std::cout << "拷贝构造被调用" << std::endl;
+	}
+	Student& operator= (const Student& other) {
+		std::cout << "拷贝赋值运算符被调用" << std::endl;
+		m_name = other.m_name;
+		m_age = other.m_age;
+		return *this;
+	}
+	~Student() {
+		std::cout << "析构被调用" << std::endl;
+	}
 private:
-    string name_;
-    int age_;
+	std::string m_name;
+	int m_age;
 };
 
+
+
 int main() {
-    Student s1("Tom", 20);
-    Student s2 = s1;
-    Student s3;
-    s3 = s1;
-    return 0;
+	Student s1("Tom", 20);
+	Student s2 = s1;     // 拷贝构造
+	Student s3;
+	s3 = s1;             // 拷贝赋值
+	return 0;
 }
+
+/*
+如果你没有写构造函数，编译器会为你生成哪些函数？
+
+会生成6种函数
+1、默认构造函数
+2、析构函数
+3、拷贝构造函数
+4、拷贝赋值函数
+5、移动构造函数
+6、移动赋值运算符
+
+构造函数和拷贝构造的本质区别是什么？
+
+构造函数是根据传入参数进行赋值构造，而拷贝构造是根据传入的对象进行赋值
+
+为什么需要显式写析构函数？什么时候会出问题？
+
+写显示析构函数可以防止在类是动态存储时候出现问题
+如在动态存储时候其中内部通过new分配的空间默认析构函数不会挥手故需要写显示析构函数
+
+*/
